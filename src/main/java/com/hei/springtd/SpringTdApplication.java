1ecc72f1eecbe471225a795f861294fa5adab8b7
Lantosoa <lantosoanir@gmail.com>
2026-03-18 19:15:55 +0100

    Initial project

---
+++ src/main/java/com/hei/springtd/SpringTdApplication.java
@@ -0,0 +1,13 @@
+package com.hei.springtd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hei/springtd/SpringTdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hei.springtd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
